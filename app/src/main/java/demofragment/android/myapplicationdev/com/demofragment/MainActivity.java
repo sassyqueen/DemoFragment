8edd8c4988ac0c85b514bc5a021d9cc40e4b1d1c
15004557 <H0peyur3gd7>
2017-06-01 10:24:53 +0800

    commit

--- app/src/main/java/demofragment/android/myapplicationdev/com/demofragment/MainActivity.java
+++ app/src/main/java/demofragment/android/myapplicationdev/com/demofragment/MainActivity.java
@@ -1,7 +1,12 @@
 package demofragment.android.myapplicationdev.com.demofragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import layout.FragmentFirst;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        Fragment f1 = new FragmentFirst();
+        ft.replace(R.id.frame1, f1);
+
+        ft.commit();
+
     }
 }
